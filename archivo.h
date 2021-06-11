edf36f17349fde8c52bc5abe42173bc3072c1f89
Maria Cecilia Valenti <macecilia717@hotmail.com>
2021-06-11 19:53:59 -0300

    Reestructuramiento 2

--- archivo.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARCHIVO_H_INCLUDED
-#define ARCHIVO_H_INCLUDED
-
-
-int escribe_archivo(char* nombreArchivo);
-int texto_a_alumno(const char* cadena_txt, Dalumno* alumno, tFecha* tfecha1);
-void graba_error(FILE* arch_error,const char* cadena_txt);
-#endif // ARCHIVO_H_INCLUDED
