1d6559f96e1b13c0cb931c41318a27167fc4e2ce
Maria Cecilia Valenti <macecilia717@hotmail.com>
2021-06-29 20:08:18 -0300

    Implementacion de Pila - primeras funiones

--- Projects/proyectoTDA/main.c
+++ Projects/proyectoTDA/main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "dpila.h"
 
 int main()
 {
-
+    int i,r;
+    tPila p;
+    crear_pila(&p);
+    for (i=0;i<15;i++)
+    {
+        r=poner_en_pila(&p,&i,sizeof(i));
+        if (!r)
+            {printf("Pila llena");}
+    }
+    while(sacar_de_pila(&p,&i,sizeof(i))){
+        printf("%d ",i);
+    }
     return 0;
 
 }
